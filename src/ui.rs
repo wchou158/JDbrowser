7d7204dfb169b09f34f741058d50242b61580de5
Jkeyuk <jonkero2@gmail.com>
2025-02-04 09:34:53 -0500

    add additional keybind tables

--- src/ui.rs
+++ src/ui.rs
@@ -92,7 +92,23 @@ const FILE_MENU_KEYS: [[&str; 2]; 3] = [["Up", "k"], ["Down", "j"], ["Select", "
 const FILE_TITLE: &str = "File Menu Keybinds";
 
 const MAIN_VIEW_TITLE: &str = "Main View Keybinds";
-const NAV_TAB: [[&str; 2]; 1] = [["Switch Table - Views", "q - e"]];
+const TAB_BAR_KEYS: [[&str; 2]; 2] = [
+    ["Switch Tables - Views", "q - e"],
+    ["Switch View Schema - Browse Data", "h - l"],
+];
+
+const NAV_LIST_TITLE: &str = "Navigation List Keybinds (Left side)";
+const NAV_LIST_KEYS: [[&str; 2]; 2] = [["Up", "k"], ["Down", "j"]];
+
+const TABLE_VIEW_TITLE: &str = "Table View Keybinds";
+const TABLE_KEYS: [[&str; 2]; 6] = [
+    ["Page Up Half", "u"],
+    ["Page Down Half", "d"],
+    ["Move Cell Up", "shift + k"],
+    ["Move Cell Down", "shift + j"],
+    ["Move Cell Left", "shift + h"],
+    ["Move Cell Right", "shift + l"],
+];
 
 fn draw_help_window(frame: &mut Frame, lay: Rect) {
     let background = Block::bordered()
@@ -102,7 +118,7 @@ fn draw_help_window(frame: &mut Frame, lay: Rect) {
     frame.render_widget(Clear, lay);
     frame.render_widget(background, lay);
 
-    let l = Layout::vertical([Constraint::Fill(1), Constraint::Fill(1)])
+    let l = Layout::vertical(Constraint::from_lengths([5, 4, 4, 10]))
         .margin(2)
         .split(lay);
 
@@ -114,10 +130,29 @@ fn draw_help_window(frame: &mut Frame, lay: Rect) {
     .block(Block::default().title(FILE_TITLE.underlined()));
     frame.render_widget(file_menu_table, l[0]);
 
-    let main_view_table = Table::new(NAV_TAB.map(|x| Row::new(x)), Constraint::from_mins([0, 0]))
-        .fg(TEXT_COLOR)
-        .block(Block::default().title(MAIN_VIEW_TITLE.underlined()));
+    let main_view_table = Table::new(
+        TAB_BAR_KEYS.map(|x| Row::new(x)),
+        Constraint::from_mins([0, 0]),
+    )
+    .fg(TEXT_COLOR)
+    .block(Block::default().title(MAIN_VIEW_TITLE.underlined()));
     frame.render_widget(main_view_table, l[1]);
+
+    let nav_list_table = Table::new(
+        NAV_LIST_KEYS.map(|x| Row::new(x)),
+        Constraint::from_mins([0, 0]),
+    )
+    .fg(TEXT_COLOR)
+    .block(Block::default().title(NAV_LIST_TITLE.underlined()));
+    frame.render_widget(nav_list_table, l[2]);
+
+    let table_view_table = Table::new(
+        TABLE_KEYS.map(|x| Row::new(x)),
+        Constraint::from_mins([0, 0]),
+    )
+    .fg(TEXT_COLOR)
+    .block(Block::default().title(TABLE_VIEW_TITLE.underlined()));
+    frame.render_widget(table_view_table, l[3]);
 }
 
 fn draw_outer_frame(frame: &mut Frame, app: &App, area: Rect) {
